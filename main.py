5f5e672c4aed8dcc161ef49b8e29d4612246e5cf
mitchelln11 <mitchellnaleid@gmail.com>
2025-05-28 10:13:54 -0500

    fix: re-order quit, cleanup

--- main.py
+++ main.py
@@ -5,15 +5,11 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
-        screen.fill(pygame.Color("black"))
-        pygame.display.flip()
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        print("Starting Asteroids!")
-        print(f"Screen width: {SCREEN_WIDTH}")
-        print(f"Screen height: {SCREEN_HEIGHT}")
+        screen.fill(pygame.Color("black"))
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
